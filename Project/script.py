d28fe8f16fe1373dd3a84a4907abd09b79b546cb
Phuoc Long <caophuoclong1@gmail.com>
2020-06-02 20:34:19 +0700

    Change

--- Project/script.py
+++ Project/script.py
@@ -17,13 +17,15 @@ def convert(messe):
         'speed': '-1',
         'voice': 'banmai'
     }
-
+    print("Long1")
     response = requests.request('POST', url, data=payload.encode('utf-8'), headers=headers)
     x = json.loads(response.text)
     link = x.get('async') # Lấy link của file mp3
+    print(link)
     page = requests.get(link) # Gửi request.
     while page.status_code == 404: # Check xem request có lỗi không
-        page = requests.get(link) 
+        page = requests.get(link)
+    print("Long2") 
     fname = "Play"+str(int(time.time()))+".wav" #Tạo tên file.
     filelo = "{}/{}".format(savefile,fname) #Đường dẫn tới file mp3
     file = open(filelo.format(fname), 'wb') #Mở file theo dạng nhị phân
@@ -33,7 +35,9 @@ def convert(messe):
     
 
     file = filelo
+    print("Long3")
     os.system("mpg123 " + file) #Dùng package mpg123 để chạy file mp3
+    print("Long4")
     os.remove(filelo) #Xóa file sau khi dùng xong
 def text(x):
     return x
@@ -42,7 +46,7 @@ def main(ms):
     file = open("history.txt","w+")
     x = "Long"
     while x != "q":
-        x = ms
+        x = input("Type Some thing: ")
         if x == "q":
             break
         if x == None:
@@ -56,9 +60,11 @@ def main(ms):
             wea = weather.main() # Đọc thời tiết tại vị trí cần thơ
             convert(wea)
         else:
-            messe = test.simsimi(x)
+            messe = test.simsimi("Xin chao ban")
+            print(messe)
             file.write(messe)
             file.close
             convert(messe)
             return messe
         break
+main("Long")
